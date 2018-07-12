9daaa1bded50d2f2cdfadf9d810a98126363a87f
Shiny Alex <carvantes.alex@gmail.com>
2018-07-12 16:20:16 -0500

    dividir index

--- header.php
+++ header.php
@@ -1,1 +1,32 @@
 <link rel="stylesheet" type="text/css" href="style.css">
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 5px 80px;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: right;
+    padding-bottom: 5px;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+
+</style>
+<header>
+    <ul>
+      <li><a href="index2.php">Gestionar datos</a></li>
+      <li><a class="active" href="index.php">Cotizar</a></li>
+      
+   </ul>
+</header>
---
+++ index2.php
@@ -0,0 +1,103 @@
+<?php
+//including the database connection file
+include_once("config.php");
+
+
+
+//fetching data in descending order (lastest entry first)
+//$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
+$result = mysqli_query($mysqli, "SELECT * FROM album ORDER BY id_album DESC"); // using mysqli_query instead
+$result_clase = mysqli_query($mysqli, "SELECT * FROM clase ORDER BY id_clase DESC");
+
+?>
+   <html>
+    <body>
+        <head>
+            <title>Gestionar Estampas</title>
+            <?php include 'header.php';?>
+        </head>
+        <section id="consultar">
+   
+            <div class="flex nowrap">
+                <div class="center col three">
+                   <h2>Album</h2>
+                    <a href="add-album.php">Agregar Album</a>
+                </div>
+
+                <div class="center col three">
+                    <form action="add-estampa.php" method="post" >
+                               <h2>Estampa</h2>
+                               <p>(elegir clase, elegir clase variable para precios únicos)</p>
+                                <?php
+                                    include_once("config.php");
+                                    $sql = "SELECT * FROM clase";
+                                    $query = mysqli_query($mysqli, $sql);
+
+                                    echo '<select name="id_clase" class="mid-space">';
+                                        while ($row = mysqli_fetch_array($query)) {
+                                            echo '<option value='.$row['id_clase'].'>'.$row['color'].'</option>';
+                                        }
+                                    echo '</select>';
+                                ?>
+                       <input type="submit" class="btn-green-small" name="class-selector" value="Añadir estampa nueva" >
+                    </form>
+                </div>
+
+                <div class="center col three">
+                   <h2>Clase</h2>
+                    <a href="add-category.php">Agregar clase</a>
+                </div>
+            </div>
+
+
+
+            <div class="flex">
+                <div class="col center three">
+                    <h2>Albums</h2>
+                    <table>
+
+                    <tr>
+                        <td>Nombre</td>
+                    </tr>
+                    <?php 
+                    //while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
+                    while($res = mysqli_fetch_array($result)) { 		
+                        echo "<tr>";
+                        echo "<td>".$res['nombre']."</td>";
+                    //id->id_album
+                    //aux=2 == borrado de album
+                        echo "<td><a href=\"edit.php?id=$res[id_album]\">Estampas</a> | <a href=\"delete.php?id=$res[id_album]&aux=2\" onClick=\"return confirm('¿Está seguro de querer eliminar?, todas las estampas del álbum serán eliminadas IRREVERSIBLEMENTE')\">Borrar</a></td>";		
+                    }
+                    ?>
+                    </table>
+                </div>
+                <div class="col center three">
+                    <h2>Clases</h2>
+                  <table>
+
+                  <tr>
+                    <td><b>Color</b></td>
+                  </tr>
+                  <?php 
+                  //while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
+                  while($res = mysqli_fetch_array($result_clase)) {  
+                  if($res['id_clase']!=-1){   
+                    echo "<tr>";
+                    echo "<td>".$res['color']."</td>";
+                    //id->id_clase
+                      echo "<td><a href=\"edit-class.php?id=$res[id_clase]\">Editar clase</a></td>";
+                    }    
+                  }
+                  ?>
+                  </table>
+                </div>
+
+                <div class="center col three">
+                   <h2>Estadísticas</h2>
+                    <a href="estadisticas.php">Más Vendidas</a>
+                </div>
+            </div>
+        </section>
+
+    </body>
+</html>
