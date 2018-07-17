c319809cdf5bbbdb756c3bc4edc46dcc32f467fe
Shiny Alex <carvantes.alex@gmail.com>
2018-07-17 12:06:40 -0500

    Navbar Fixed

--- header.php
+++ header.php
@@ -1,32 +1,17 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 <style>
-ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 5px 80px;
-    overflow: hidden;
-    background: #1d398d;
-}
 
-li {
-    float: right;
-    padding-bottom: 5px;
-}
-
-li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-}
 
 
 </style>
 <header>
-    <ul>
-      <li><a href="index2.php">Gestionar datos</a></li>
-      <li><a class="active" href="index.php">Cotizar</a></li>
-      
-   </ul>
+   <div class="navbar">
+       <ul>
+          <li><a href="index2.php">Gestionar datos</a></li>
+          <li><a class="active" href="index.php">Cotizar</a></li>
+
+       </ul>
+       
+   </div>
+    <br><br><br>
 </header>
