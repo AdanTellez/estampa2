818cb6ae4f6b63c11a77112bdec94655aab3e7c7
Shiny Alex <carvantes.alex@gmail.com>
2018-07-17 21:43:50 -0500

    JSGRID alpha

--- header.php
+++ header.php
@@ -1,9 +1,13 @@
 <link rel="stylesheet" type="text/css" href="style.css">
-<style>
+<link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.css" />
+<link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid-theme.min.css" />
+
+   
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.js"></script>
 
 
-
-</style>
 <header>
    <div class="navbar">
        <ul>
